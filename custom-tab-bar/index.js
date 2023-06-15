44af1695711de6cfe99c32863d65130abf9ebe5d
EricZhouXiao <30684350+EricZhouXiao@users.noreply.github.com>
2023-06-15 12:05:11 -0400

    tabbar UI
    tab-bar ui移植， 逻辑层和ui细节未完成

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,49 @@
+getApp();
+
+Component({
+    data: {
+        selected: 0,
+        sysMessageCount: 0,
+        color: "#949494",
+        selectedColor: "#333333",
+        fontWeight: "bold",
+        list: [ {
+            pagePath: "/pages/tabbar/index/index",
+            iconPath: "/image/tabbar/tab_index_normal.png",
+            selectedIconPath: "/image/tabbar/tab_index_active.png",
+            text: "首页"
+        }, {
+            /*pagePath: "/pages/tabbar/circle/circle",
+            iconPath: "/image/tabbar/tab_nearby_normal.png",
+            selectedIconPath: "/image/tabbar/tab_nearby_active.png",*/
+            text: "圈子"
+        }, {
+           /* pagePath: "/pages/creat/index/creat",
+            iconPath: "/image/tabbar/icon_add.png",
+            selectedIconPath: "/image/tabbar/icon_add.png",*/
+            text: "",
+            isSpecial: !0
+        }, {
+            /*pagePath: "/pages/tabbar/shop/shop",
+            iconPath: "/image/tabbar/tab_shop_normal.png",
+            selectedIconPath: "/image/tabbar/tab_shop_active.png",*/
+            text: "SHOP"
+        }, {
+            pagePath: "/pages/tabbar/mine/mine",
+            iconPath: "/image/tabbar/tab_user_normal.png",
+            selectedIconPath: "/image/tabbar/tab_user_active.png",
+            text: "我家"
+        } ]
+    },
+    attached: function() {},
+    methods: {
+        switchTab: function(a) {
+            var t = a.currentTarget.dataset.index, e = a.currentTarget.dataset.url;
+            this.data.list[t].isSpecial ? wx.navigateTo({
+                url: e
+            }) : wx.switchTab({
+                url: e
+            });
+        }
+    }
+});
