dfb45fc096543ba390496845e57558c616f26d4c
Snake Amadeus <snake4y5h@gmail.com>
2024-03-26 22:44:58 -0400

    Fix Detail Page Typo

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -74,7 +74,7 @@ Page({
                         ddl_forDisplay: dateToDotFormat(res.data.expiration_date),
                         image_urls: res.data.image_urls,
                         post_status: res.data.post_status,
-                        contact_info: "微信号: " + res.data.contact_info["wechat_id"] + "邮箱：" + res.data.contact_info["email_address"]
+                        contact_info: "微信号: " + res.data.contact_info["wechat_id"] + "邮箱：" + res.data.contact_info["email_address"],
                         post_type: res.data.post_type
                     });
                     console.log(that.data.post_type)
